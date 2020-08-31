69c65d62c58214a2c132ecb0c8a04a5cd9c661b8
hsLee <gntmddl4261@gmail.com>
2020-08-31 23:40:34 +0900

    ㅁㅁ

--- bitcamp/src/kr/co/bit_camp/attcontroller/AttController.java
+++ bitcamp/src/kr/co/bit_camp/attcontroller/AttController.java
@@ -19,7 +19,7 @@ public class AttController implements Controller {
 	//되돌린뒤다시커밋
 	//데스크탑에서푸쉬
 	//노트북에서 푸쉬
-	//노트북에서 푸쉬2 ㅋㅋㅋㅋㅋㅋㅇㅇㅇㄴㅁㅇㄴㅁㅇㅁㄴㅇㄴㅁㅇㅁㄴㅇ
+	//노트북에서 푸쉬2 ㅋㅋㅋㅋㅋㅋㅇㅇㅇㄴㅁㅇㄴㅁㅇㅁㄴㅇㄴㅁㅇㅁㄴㅇㅁㅁ
 	Scanner sc = new Scanner(System.in);
 	Calendar c = Calendar.getInstance();
 	private AttendMapper mapper;
