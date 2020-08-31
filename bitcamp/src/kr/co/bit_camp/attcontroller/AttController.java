1b5176934d31a07e08de27b6a4447f741955a8b5
hsLee <gntmddl4261@gmail.com>
2020-08-31 23:37:32 +0900

    커밋

--- bitcamp/src/kr/co/bit_camp/attcontroller/AttController.java
+++ bitcamp/src/kr/co/bit_camp/attcontroller/AttController.java
@@ -16,10 +16,7 @@ import kr.co.bit_camp.usercontroller.LogInController;
 import kr.co.bit_camp.vo.Attendence;
 
 public class AttController implements Controller {
-	//되돌린뒤다시커밋
-	//데스크탑에서푸쉬
-	//노트북에서 푸쉬
-	//노트북에서 푸쉬2 ㅋㅋㅋㅋㅋㅋㅇㅇㅇㄴㅁㅇㄴㅁㅇㅁㄴㅇㄴㅁㅇㅁㄴㅇ
+	//hslee브랜치
 	Scanner sc = new Scanner(System.in);
 	Calendar c = Calendar.getInstance();
 	private AttendMapper mapper;
